fae14eb8a3422b0561f4da46625dc47f108bd93b
acheronfail <acheronfail@gmail.com>
2022-04-28 21:30:35 +0930

    tentative attempts at tree modification

--- r3/src/tiler/mod.rs
+++ r3/src/tiler/mod.rs
@@ -18,78 +18,25 @@ pub struct Tree<B, L> {
     root: Node<B, L>,
 }
 
-impl<B: Default, L> Tree<B, L> {
-    pub fn new() -> Self {
-        Self {
-            root: Node::leaves(B::default()),
-        }
-    }
-}
-
 impl<B: Display, L: Display> Tree<B, L> {
     pub fn write_tree(f: &mut dyn std::fmt::Write, tree: &Tree<B, L>) -> std::fmt::Result {
         writeln!(f, "Root")?;
-        Self::write_tree_inner(f, &tree.root, vec![1])
-    }
-
-    fn write_tree_inner(f: &mut dyn std::fmt::Write, node: &Node<B, L>, depths: Vec<usize>) -> std::fmt::Result {
-        const EMPTY: &str = "   ";
-        const EDGE: &str = " └─";
-        const PIPE: &str = " │ ";
-        const BRANCH: &str = " ├─";
-
-        // The connection to this node
-        let mut this_prefix = String::new();
-        // The connection to siblings of this node
-        let mut sibling_prefix = String::new();
-
-        // Iterate through the current depths to build up the right prefixes
-        for (i, depth) in depths.iter().enumerate() {
-            let last = i == depths.len() - 1;
-            if *depth == 1 {
-                this_prefix.push_str(if last { EDGE } else { EMPTY });
-                sibling_prefix.push_str(EMPTY);
-            } else {
-                this_prefix.push_str(if last { BRANCH } else { PIPE });
-                sibling_prefix.push_str(PIPE);
-            }
-        }
-
-        write!(f, "{}", this_prefix)?;
-        match node {
-            Node::Branch { children, inner } => {
-                let mut depth = children.len();
-                writeln!(f, "Branch({})", inner)?;
-                for n in children {
-                    let mut next_depths = depths.clone();
-                    next_depths.push(depth);
-                    depth -= 1;
-                    Self::write_tree_inner(f, n, next_depths)?;
-                }
-            }
-            Node::Leaves { children, inner } => {
-                writeln!(f, "Leaves({})", inner)?;
-                for (i, leaf) in children.iter().enumerate() {
-                    let last = i == children.len() - 1;
-                    writeln!(f, "{}{}{}", sibling_prefix, if last { EDGE } else { BRANCH }, leaf)?;
-                }
-            }
-        }
-
-        Ok(())
+        Node::write_tree(f, &tree.root, Some("Root"))
     }
 }
 
 impl<B: Display, L: Display> ToString for Tree<B, L> {
     fn to_string(&self) -> String {
         let mut s = String::new();
-        Tree::write_tree(&mut s, self).expect("Failed to convert Tree to String!");
+        Node::write_tree(&mut s, &self.root, Some("Root")).expect("Failed to convert Tree to String!");
         s
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::sync::{Arc, Weak};
+
     use indoc::indoc;
 
     use self::node::*;
@@ -100,7 +47,8 @@ mod tests {
         // TODO: easier ways to build these
         let tree = Tree {
             root: Node::Leaves {
-                inner: 'a',
+                parent: Weak::new(), /* TODO */
+                inner: Some('a'),
                 children: vec![Leaf(0), Leaf(1), Leaf(2)],
             },
         };
@@ -117,20 +65,24 @@ mod tests {
 
         let tree = Tree {
             root: Node::Branch {
-                inner: 'a',
+                parent: Weak::new(), /* TODO */
+                inner: Some('a'),
                 children: vec![
-                    Node::Leaves {
-                        inner: 'b',
+                    Arc::new(Node::Leaves {
+                        parent: Weak::new(), /* TODO */
+                        inner: Some('b'),
                         children: vec![Leaf(0), Leaf(1)],
-                    },
-                    Node::Leaves {
-                        inner: 'c',
+                    }),
+                    Arc::new(Node::Leaves {
+                        parent: Weak::new(), /* TODO */
+                        inner: Some('c'),
                         children: vec![Leaf(2)],
-                    },
-                    Node::Leaves {
-                        inner: 'd',
+                    }),
+                    Arc::new(Node::Leaves {
+                        parent: Weak::new(), /* TODO */
+                        inner: Some('d'),
                         children: vec![Leaf(3), Leaf(4), Leaf(5)],
-                    },
+                    }),
                 ],
             },
         };
@@ -153,26 +105,32 @@ mod tests {
 
         let tree = Tree {
             root: Node::Branch {
-                inner: 'a',
+                parent: Weak::new(), /* TODO */
+                inner: Some('a'),
                 children: vec![
-                    Node::Branch {
-                        inner: 'b',
-                        children: vec![Node::Leaves {
-                            inner: 'c',
+                    Arc::new(Node::Branch {
+                        parent: Weak::new(), /* TODO */
+                        inner: Some('b'),
+                        children: vec![Arc::new(Node::Leaves {
+                            parent: Weak::new(), /* TODO */
+                            inner: Some('c'),
                             children: vec![Leaf(0), Leaf(1), Leaf(2)],
-                        }],
-                    },
-                    Node::Leaves {
-                        inner: 'd',
+                        })],
+                    }),
+                    Arc::new(Node::Leaves {
+                        parent: Weak::new(), /* TODO */
+                        inner: Some('d'),
                         children: vec![Leaf(3), Leaf(4)],
-                    },
-                    Node::Branch {
-                        inner: 'e',
-                        children: vec![Node::Leaves {
-                            inner: 'f',
+                    }),
+                    Arc::new(Node::Branch {
+                        parent: Weak::new(), /* TODO */
+                        inner: Some('e'),
+                        children: vec![Arc::new(Node::Leaves {
+                            parent: Weak::new(), /* TODO */
+                            inner: Some('f'),
                             children: vec![Leaf(5), Leaf(6)],
-                        }],
-                    },
+                        })],
+                    }),
                 ],
             },
         };
